2e8ec2819fb8ae45dfca55e93969091f27d21c47
Thasi <thasithat7@gmail.com>
2024-02-20 16:32:20 +0530

    Raj update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="keywords" content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern,  html5, responsive">
     <meta name="author" content="Dreamguys - Bootstrap Admin Template">
     <meta name="robots" content="noindex, nofollow">
-    <title>Erikanesh Pos Software</title>
+    <title>Erikanesh Pos Software BY Thasitha</title>
 
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
 
